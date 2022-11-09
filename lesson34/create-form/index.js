15116496a0407da2cbd31de840bb8081a9635b72
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-09 17:21:50 +0200

    feat: lesson34 task create-form

---
+++ lesson34/create-form/index.js
@@ -0,0 +1,44 @@
+const _baseUrl = 'https://636a8bfbc07d8f936da160be.mockapi.io/api/v1/users';
+
+const _form = document.forms[0];
+const _FIELDS_FORM_AMOUNT = [..._form.querySelectorAll('input')].length;
+const _statesValidationForm = {};
+
+const _onInput = event => {
+  if (Object.keys(_statesValidationForm).length === _FIELDS_FORM_AMOUNT) {
+    document.querySelector('button').removeAttribute('disabled');
+  } else if (event.target.reportValidity()) {
+    _statesValidationForm[event.target.name] = true;
+  }
+};
+
+const _createUser = data =>
+  fetch(_baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  });
+
+const _onSubmitForm = event => {
+  event.preventDefault();
+
+  const data = Array.from(new FormData(event.target)).reduce(
+    (acc, item) => ({ ...acc, [item[0]]: item[1] }),
+    {},
+  );
+  _createUser(data).then(response => {
+    event.target.reset();
+    console.log(`Status of response ${response.status}`);
+  });
+};
+
+const initHandlers = () => {
+  [..._form.querySelectorAll('input')].forEach(element =>
+    element.addEventListener('input', _onInput),
+  );
+  _form.addEventListener('submit', _onSubmitForm);
+};
+
+document.addEventListener('DOMContentLoaded', initHandlers);
