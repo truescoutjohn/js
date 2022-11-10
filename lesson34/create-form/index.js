90f4d8ad3403471208a5e38e6ded81fbbb2e7fda
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-11-10 10:44:42 +0200

    fix: final solution

--- lesson34/create-form/index.js
+++ lesson34/create-form/index.js
@@ -5,34 +5,90 @@
 //   2.2. Create form data
 //   2.3. Make fetch request
 // 3. Add event to inputs and form
+// const _baseUrl = 'https://636a8bfbc07d8f936da160be.mockapi.io/api/v1/users';
+
+// const _form = document.querySelector('.login-form');
+// const _FIELDS_FORM_AMOUNT = _form.querySelectorAll('.form-input').length;
+// const _statesValidationForm = {};
+// const _button = document.querySelector('.submit-button');
+
+// const _setValidationFlagToObject = input => {
+//   if (input.reportValidity()) {
+//     _statesValidationForm[input.name] = true;
+//   } else {
+//     _statesValidationForm[input.name] = false;
+//   }
+// };
+
+// const _setButtonDisabled = (button, states) => {
+//   if (states.every(item => item) && states.length === _FIELDS_FORM_AMOUNT) {
+//     button.removeAttribute('disabled');
+//   } else {
+//     button.setAttribute('disabled', '');
+//   }
+// };
+
+// const _onInput = event => {
+//   _setValidationFlagToObject(event.target);
+//   const states = Object.values(_statesValidationForm);
+//   _setButtonDisabled(_button, states);
+// };
+
+// const _createUser = data =>
+//   fetch(_baseUrl, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json;charset=utf-8',
+//     },
+//     body: JSON.stringify(data),
+//   }).then(response => response.json());
+
+// const _resetFlagsObject = () => {
+//   Object.keys(_statesValidationForm).forEach(key => {
+//     _statesValidationForm[key] = false;
+//   });
+// };
+// const _onSubmitForm = event => {
+//   event.preventDefault();
+
+//   const data = Array.from(new FormData(event.target)).reduce(
+//     (acc, item) => ({ ...acc, [item[0]]: item[1] }),
+//     {},
+//   );
+
+//   _createUser(data).then(task => {
+//     event.target.reset();
+//     alert(JSON.stringify(task));
+//     _button.setAttribute('disabled', '');
+//     _resetFlagsObject();
+//   });
+// };
+
+// const initHandlers = () => {
+//   [..._form.querySelectorAll('.form-input')].forEach(element =>
+//     element.addEventListener('input', _onInput),
+//   );
+//   _form.addEventListener('submit', _onSubmitForm);
+// };
+
+// // document.addEventListener('DOMContentLoaded', initHandlers);
+// initHandlers();
+
 const _baseUrl = 'https://636a8bfbc07d8f936da160be.mockapi.io/api/v1/users';
 
-const _form = document.querySelector('.login-form');
-const _FIELDS_FORM_AMOUNT = _form.querySelectorAll('.form-input').length;
-const _statesValidationForm = {};
-const _button = document.querySelector('.submit-button');
+const formElem = document.querySelector('.login-form');
+const submitBtnElem = document.querySelector('.submit-button');
 
-const _setValidationFlagToObject = input => {
-  if (input.reportValidity()) {
-    _statesValidationForm[input.name] = true;
+const _onInput = () => {
+  const isValidForm = formElem.reportValidity();
+  if (isValidForm) {
+    submitBtnElem.removeAttribute('disabled');
   } else {
-    _statesValidationForm[input.name] = false;
+    submitBtnElem.setAttribute('disabled', true);
   }
 };
 
-const _setButtonDisabled = (button, states) => {
-  if (states.every(item => item) && states.length === _FIELDS_FORM_AMOUNT) {
-    button.removeAttribute('disabled');
-  } else {
-    button.setAttribute('disabled', '');
-  }
-};
-
-const _onInput = event => {
-  _setValidationFlagToObject(event.target);
-  const states = Object.values(_statesValidationForm);
-  _setButtonDisabled(_button, states);
-};
+formElem.addEventListener('input', _onInput);
 
 const _createUser = data =>
   fetch(_baseUrl, {
@@ -41,13 +97,8 @@ const _createUser = data =>
       'Content-Type': 'application/json;charset=utf-8',
     },
     body: JSON.stringify(data),
-  }).then(response => response.json());
-
-const _resetFlagsObject = () => {
-  Object.keys(_statesValidationForm).forEach(key => {
-    _statesValidationForm[key] = false;
   });
-};
+
 const _onSubmitForm = event => {
   event.preventDefault();
 
@@ -55,21 +106,12 @@ const _onSubmitForm = event => {
     (acc, item) => ({ ...acc, [item[0]]: item[1] }),
     {},
   );
-
-  _createUser(data).then(task => {
-    event.target.reset();
-    alert(JSON.stringify(task));
-    _button.setAttribute('disabled', '');
-    _resetFlagsObject();
-  });
+  _createUser(data)
+    .then(response => response.json())
+    .then(task => {
+      event.target.reset();
+      alert(JSON.stringify(task));
+    });
 };
 
-const initHandlers = () => {
-  [..._form.querySelectorAll('.form-input')].forEach(element =>
-    element.addEventListener('input', _onInput),
-  );
-  _form.addEventListener('submit', _onSubmitForm);
-};
-
-// document.addEventListener('DOMContentLoaded', initHandlers);
-initHandlers();
+formElem.addEventListener('submit', _onSubmitForm);
